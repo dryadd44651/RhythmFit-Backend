d28877bd0471d307f25bbc94e1bc7e2c1cd4fcb8
Howard <dryadd44651@gmail.com>
2024-11-30 16:16:58 -0800

    update CORS_ALLOWED_ORIGINS

--- backend/settings.py
+++ backend/settings.py
@@ -137,7 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://rhythmfit-frontend-ed72d71bd3df.herokuapp.com/",
+    "https://rhythmfit-frontend-ed72d71bd3df.herokuapp.com",
 ]
 
 CORS_ALLOW_METHODS = [
